3b05f64cad08a961ea0cf06fe00cec2050a6984e
clauries <55369403+clauries@users.noreply.github.com>
2020-02-06 20:40:18 -0800

    Testing functionality of Travis linting

--- config/database.js
+++ config/database.js
@@ -14,3 +14,30 @@ module.exports = new Sequelize("star_db", "root", "Gobruins2020", {
     idle: 10000
   }
 });
+
+
+
+// Test code for Travis
+var num1 = parseInt(prompt("Give me a number!"));
+var num2 = parseInt(prompt("Give me another number!"));
+var result;
+
+var operation = prompt("What would you like to do? (add, subtract, multiply, divide)").toUpperCase();
+
+if (operation === "ADD") {
+  result = num1 + num2;
+  alert("The sum of " + num_1 + " and " + num_2 + " is " + result);
+} else if (operation === "SUBTRACT") 
+{
+result = num_1 - num_2;
+alert('The difference between ' + num_1 + " and " + num_2 + " is " + result);
+} 
+else if (operation === "MULTIPLY") {
+    result = num_1 * num_2;
+    alert('The product of ' + num_1 + " and " + num_2 + " is " + result);
+} else if (operation === "DIVIDE") {
+result = num_1 / num_2;
+  alert('The quotient of ' + num_1 + " and " + num_2 + " is " + result);
+} else {
+alert("Not a valid option!");
+} 
