dd455e89bdf56d8c1b9ff5af14b474c4dc95ae13
jaredbucko <jaredbucko@icloud.com>
2020-02-06 21:19:19 -0800

    update files in db

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,14 @@
-DROP DATABASE IF EXISTS star_db;
+DROP DATABASE IF EXISTS starGazer_db;
 
-CREATE database star_db;
+CREATE database starGazer_db;
 
-USE star_db;
+USE starGazer_db;
+
+CREATE TABLE constellations (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NULL,
+  abbreviation VARCHAR(255) NULL,
+  origin VARCHAR(255) NULL,
+  meaning VARCHAR(255) NULL,
+  PRIMARY KEY (id)
+);
---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+USE starGazer_db;
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Andromeda", "Andr", "ancient", "Andromeda (The chained maiden or princess)");
