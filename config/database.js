700ce42ce3fb67c5bf9811c4c0b46a62e1a71111
clauries <55369403+clauries@users.noreply.github.com>
2020-02-06 19:02:49 -0800

    Making sure I can push to github

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 
 //////////////////////////////////////////////////enter your user & password
 
-module.exports = new Sequelize("star_db", "root", "1982", {
+module.exports = new Sequelize("star_db", "root", "si11y#b0y", {
   host: "localhost",
   dialect: "mysql",
   operatorsAliases: false,
