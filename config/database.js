433c5a6d91330dece6f5a79026c1882416a6e644
alpinelife37 <54160490+alpinelife37@users.noreply.github.com>
2020-02-06 18:34:00 -0800

    updated gitiqnore

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const Sequelize = require("sequelize");
+
+//////////////////////////////////////////////////enter your user & password
+
+module.exports = new Sequelize("star_db", "root", "1982", {
+  host: "localhost",
+  dialect: "mysql",
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
