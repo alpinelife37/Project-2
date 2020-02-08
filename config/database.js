8e9ce69d4bffe2da06b3b97ae1cb3e2175968732
Elizabeth Garcia-Quilinquin <equilinquin@gmail.com>
2020-02-08 10:57:24 -0800

    Changed name of model.  Changed database name in database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 // ////////////////////////////////////////////////    enter your user & password
 
-module.exports = new Sequelize('star_db', 'root', '1982', {
+module.exports = new Sequelize('starGazer_db', 'root', '1982', {
   host: 'localhost',
   dialect: 'mysql',
   operatorsAliases: false,
@@ -14,30 +14,3 @@ module.exports = new Sequelize('star_db', 'root', '1982', {
     idle: 10000,
   },
 });
-
-
-
-// Test code for Travis
-var num1 = parseInt(prompt("Give me a number!"));
-var num2 = parseInt(prompt("Give me another number!"));
-var result;
-
-var operation = prompt("What would you like to do? (add, subtract, multiply, divide)").toUpperCase();
-
-if (operation === "ADD") {
-  result = num1 + num2;
-  alert("The sum of " + num_1 + " and " + num_2 + " is " + result);
-} else if (operation === "SUBTRACT") 
-{
-result = num_1 - num_2;
-alert('The difference between ' + num_1 + " and " + num_2 + " is " + result);
-} 
-else if (operation === "MULTIPLY") {
-    result = num_1 * num_2;
-    alert('The product of ' + num_1 + " and " + num_2 + " is " + result);
-} else if (operation === "DIVIDE") {
-result = num_1 / num_2;
-  alert('The quotient of ' + num_1 + " and " + num_2 + " is " + result);
-} else {
-alert("Not a valid option!");
-} 
--- models/constellations.js
+++ models/constellations.js
@@ -1,2 +1,6 @@
 const Sequelize = require("sequelize");
 const db = require("../config/database");
+
+
+
+module.exports = Constellations;
