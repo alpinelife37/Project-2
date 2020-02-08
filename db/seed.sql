65e43905232440ba8635ccfe63fa59493adbc44f
jaredbucko <jaredbucko@icloud.com>
2020-02-08 11:06:42 -0800

    update seed file

--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,265 @@
 USE starGazer_db;
 
 INSERT INTO constellations (name, abbreviation, origin, meaning)
-VALUES ("Andromeda", "Andr", "ancient", "Andromeda (The chained maiden or princess)");
+VALUES ("Andromeda", "Andr", "ancient", "Daughter of Cassiopeia");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Antlia", "Antl", "ancient", "The Air Pump");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Apus", "Apus", "ancient", "Bird of Paradise");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Aquarius", "Aqar", "ancient", "Daughter of Cassiopeia");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Aquila", "Aqil", "ancient", "Daughter of Cassiopeia");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Ara", "Arae", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Aries", "Arie", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Auriga", "Auri", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Boötes", "Boot", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Caelum", "Cael", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Camelopardalis", "Caml", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Cancer", "Canc", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Canes Venatici", "CVen", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Canis Major", "CMaj", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Canis Minor", "CMin", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Capricornus", "Capr", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Carina", "Cari", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Cassiopeia", "Cass", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Centaurus", "Cent", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Cepheus", "Ceph", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Cetus", "Ceti", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Chamaeleon", "Cham", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Circinus", "Circ", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Columba", "Colm", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Coma Berenices", "Coma", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Corona Australis", "CorA", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Corona Borealis", "CorB", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Corvus", "Corv", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Crater", "Crat", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Crux", "Cruc", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Cygnus", "Cygn", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Delphinus", "Diph", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Dorado", "Dora", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Draco", "Drac", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Equuleus", "Equl", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Eridanus", "Erid", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Fornax", "Forn", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Gemini", "Gemi", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Grus", "Grus", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Hercules", "Herc", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Horologium", "Horo", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Hydra", "Hyda", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Hydrus", "Hydi", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Indus", "Indi", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Lacerta", "Lacr", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Leo", "Leon", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Leo Minor", "LMin", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Lepus", "Leps", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Libra", "Libr", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Lupus", "Lupi", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Lynx", "Lync", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Lyra", "Lyra", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Mensa", "Mens", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Microscopium", "Micr", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Monoceros", "Mono", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Musca", "Musc", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Norma", "Norm", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Octans", "Octn", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Ophiuchus", "Ophi", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Orion", "Orio", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Pavo", "Pavo", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Pegasus", "Pegs", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Perseus", "Pers", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Phoenix", "Phoe", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Pictor", "Pict", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Pisces", "Pisc", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Piscis Austrinus", "PscA", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Puppis", "Pupp", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Pyxis", "Pyxi", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Reticulum", "Reti", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Sagitta", "Sgte", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Sagittarius", "Sgtr", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Scorpius", "Scor", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Sculptor", "Scul", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Scutum", "Scut", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Serpens", "Serp", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Sextans", "Sext", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Taurus", "Taur", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Telescopium", "Tele", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Triangulum", "Tria", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Triangulum Australe", "TrAu", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Tucana", "Tucn", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Ursa Major", "UMaj", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Ursa Minor", "UMin", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Vela", "Velr", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Virgo", "Virg", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Volans", "Volans", "ancient", "Andromeda (The chained maiden or princess)");
+
+INSERT INTO constellations (name, abbreviation, origin, meaning)
+VALUES ("Vulpecula", "Vulp", "ancient", "Andromeda (The chained maiden or princess)");
