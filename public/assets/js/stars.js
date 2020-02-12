e0e8230a992a07182f336d5c95fa89d2549bb9ea
clauries <55369403+clauries@users.noreply.github.com>
2020-02-11 20:12:12 -0800

    Created navbar, set up handlebars format

--- public/assets/js/stars.js
+++ public/assets/js/stars.js
@@ -1,4 +1,11 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(document).ready(function() {
+  // This file just does a GET request to figure out which user is logged in
+  // and updates the HTML on the page
+  $.get("/api/user_data").then(function(data) {
+    $(".member-name").text(data.email);
+  });
+
 $(function() {
   $(".change-sleep").on("click", function(event) {
     var id = $(this).data("id");
@@ -57,4 +64,6 @@ $(function() {
       }
     );
   });
+});
+
 });
