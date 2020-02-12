02f22ea32ce4e30126bad99361e6554685e28f4c
Elizabeth Garcia-Quilinquin <equilinquin@gmail.com>
2020-02-11 17:04:27 -0800

    Changed varibles in constControllers.js

--- controllers/constController.js
+++ controllers/constController.js
@@ -7,17 +7,17 @@ var Constellation = require("../models/constellations.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  cat.all(function(data) {
+  Constellation.all(function(data) {
     var hbsObject = {
-      cats: data
+      constellations: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
 
-router.post("/api/cats", function(req, res) {
-  cat.create(["name", "sleepy"], [req.body.name, req.body.sleepy], function(
+router.post("/api/users", function(req, res) {
+  Constellation.create(["name", "email"], [req.body.name, req.body.email], function(
     result
   ) {
     // Send back the ID of the new quote
@@ -25,39 +25,39 @@ router.post("/api/cats", function(req, res) {
   });
 });
 
-router.put("/api/cats/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+// router.put("/api/cats/:id", function(req, res) {
+//   var condition = "id = " + req.params.id;
 
-  console.log("condition", condition);
+//   console.log("condition", condition);
 
-  cat.update(
-    {
-      sleepy: req.body.sleepy
-    },
-    condition,
-    function(result) {
-      if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
-    }
-  );
-});
+//   cat.update(
+//     {
+//       sleepy: req.body.sleepy
+//     },
+//     condition,
+//     function(result) {
+//       if (result.changedRows == 0) {
+//         // If no rows were changed, then the ID must not exist, so 404
+//         return res.status(404).end();
+//       } else {
+//         res.status(200).end();
+//       }
+//     }
+//   );
+// });
 
-router.delete("/api/cats/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+// router.delete("/api/cats/:id", function(req, res) {
+//   var condition = "id = " + req.params.id;
 
-  cat.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
+//   cat.delete(condition, function(result) {
+//     if (result.affectedRows == 0) {
+//       // If no rows were changed, then the ID must not exist, so 404
+//       return res.status(404).end();
+//     } else {
+//       res.status(200).end();
+//     }
+//   });
+// });
 
 // Export routes for server.js to use.
 module.exports = router;
