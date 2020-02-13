af924310085c216161ef6afb28dfef871dd032d1
Elizabeth Garcia-Quilinquin <equilinquin@gmail.com>
2020-02-12 16:46:37 -0800

    added routes folder

--- models/user.js
+++ models/user.js
@@ -1,15 +1,33 @@
+var bcrypt = require("bcryptjs");
+
 module.exports = function(sequelize, DataTypes) {
-  const User = sequelize.define("users", {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
+  var User = sequelize.define("User", {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    // The password cannot be null
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   });
-
-  User.associate = function(models) {
-    User.hasMany(models.Constellation, {
-      onDelete: "cascade"
-    });
+  // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+  User.prototype.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
   };
-
+  // Hooks are automatic methods that run during various phases of the User Model lifecycle
+  // In this case, before a User is created, we will automatically hash their password
+  User.addHook("beforeCreate", function(user) {
+    user.password = bcrypt.hashSync(
+      user.password,
+      bcrypt.genSaltSync(10),
+      null
+    );
+  });
   return User;
 };
