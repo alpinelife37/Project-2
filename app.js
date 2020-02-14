8b214199e88dc677d8f0fdb5a4a81caef926870c
alpinelife37 <54160490+alpinelife37@users.noreply.github.com>
2020-02-13 16:54:01 -0800

    updated app.js console log to "clickable" port msg

--- app.js
+++ app.js
@@ -45,6 +45,6 @@ require("./routes/html-routes.js")(app);
 // =============================================================
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log("App listening on : http://localhost:" + PORT);
   });
 });
