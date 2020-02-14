118db89187a6ec15096be0bdeaa669ee5d06a5ed
Elizabeth Garcia-Quilinquin <equilinquin@gmail.com>
2020-02-13 21:38:16 -0800

    fixed jquery

--- public/assets/js/stars.js
+++ public/assets/js/stars.js
@@ -1,15 +1,22 @@
 console.log("hello world");
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(document).ready(function() {
-  // function getConstellations() {
-  //   const newOp = $("<option>");
-  //   $.get("/api/constellations", function(data) {
-  //     const optionsToAdd = [];
-  //     for (var i = 0; i < data.length; i++) {
-  //       optionsToAdd.push(createConstellationOption(data[i.name]));
-  //     }
-  //     renderAuthorList(rowsToAdd);
-  //     nameInput.val("");
-  //   });
-  // }
+  function getConstellations() {
+    //const newOp = $("<option>");
+
+    $.ajax({
+      method: "GET",
+      url: "/api/constellations"
+    }).then(name => {
+      console.log(name);
+    });
+    // $.get("/api/constellations", function(data) {
+    //   console.log(data);
+    //   const optionsToAdd = [];
+    //   for (var i = 0; i < data.length; i++) {
+    //     optionsToAdd.push(newOp(data[i.name]));
+    //   }
+    // });
+  }
+  getConstellations();
 });
