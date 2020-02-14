351b86417db62143574199b8ed95df2cfe386a27
clauries <55369403+clauries@users.noreply.github.com>
2020-02-13 18:12:59 -0800

    Pushing before merging master into my branch

--- public/assets/js/stars.js
+++ public/assets/js/stars.js
@@ -6,62 +6,18 @@ $(document).ready(function() {
   $.get("/api/user_data").then(function(data) {
     $(".member-name").text(data.email);
   });
-
-  $(function() {
-    $(".change-sleep").on("click", function() {
-      var id = $(this).data("id");
-      var newSleep = $(this).data("newsleep");
-
-      var newSleepState = {
-        sleepy: newSleep
-      };
-
-      // Send the PUT request.
-      $.ajax("/api/cats/" + id, {
-        type: "PUT",
-        data: newSleepState
-      }).then(function() {
-        console.log("changed sleep to", newSleep);
-        // Reload the page to get the updated list
-        location.reload();
-      });
-    });
-
-    $(".create-form").on("submit", function(event) {
-      // Make sure to preventDefault on a submit event.
-      event.preventDefault();
-
-      var newCat = {
-        name: $("#ca")
-          .val()
-          .trim(),
-        sleepy: $("[name=sleepy]:checked")
-          .val()
-          .trim()
-      };
-
-      // Send the POST request.
-      $.ajax("/api/cats", {
-        type: "POST",
-        data: newCat
-      }).then(function() {
-        console.log("created new cat");
-        // Reload the page to get the updated list
-        location.reload();
-      });
-    });
-
-    $(".delete-cat").on("click", function() {
-      var id = $(this).data("id");
-
-      // Send the DELETE request.
-      $.ajax("/api/cats/" + id, {
-        type: "DELETE"
-      }).then(function() {
-        console.log("deleted cat", id);
-        // Reload the page to get the updated list
-        location.reload();
-      });
-    });
-  });
 });
+
+/* Pseudocoding
+On load 
+  loop through db
+  create tags for each object.name
+
+On search 
+  get reqest for object with object.name === name chosen
+    Populate in const-info
+  
+  get request to 3rd party api
+    Add name chosen to https://starregistration.net/media/wysiwyg/Constellations/${`name chosen`}.png(601 kB)
+    Populate in const-img
+*/
