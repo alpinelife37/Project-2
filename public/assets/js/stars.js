f56f6c78506827d042ee33c0a63990c1ac1537c4
jaredbucko <jaredbucko@icloud.com>
2020-02-13 20:34:17 -0800

    commented out stars.js

--- public/assets/js/stars.js
+++ public/assets/js/stars.js
@@ -1,18 +1,15 @@
 console.log("hello world");
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(document).ready(function() {
-  
-
-
-  function getConstellations() {
-    const newOp = $("<option>");
-    $.get("/api/constellations", function(data) {
-      const optionsToAdd = [];
-      for (var i = 0; i < data.length; i++) {
-        optionsToAdd.push(createConstellationOption(data[i.name]));
-      }
-      renderAuthorList(rowsToAdd);
-      nameInput.val("");
-    });
-  }
+  // function getConstellations() {
+  //   const newOp = $("<option>");
+  //   $.get("/api/constellations", function(data) {
+  //     const optionsToAdd = [];
+  //     for (var i = 0; i < data.length; i++) {
+  //       optionsToAdd.push(createConstellationOption(data[i.name]));
+  //     }
+  //     renderAuthorList(rowsToAdd);
+  //     nameInput.val("");
+  //   });
+  // }
 });
