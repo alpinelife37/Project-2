cab1fcaa118773df952f7c423473dcd90d78d52a
clauries <55369403+clauries@users.noreply.github.com>
2020-02-15 10:04:31 -0800

    Relinted

--- public/assets/js/stars.js
+++ public/assets/js/stars.js
@@ -1,5 +1,5 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(document).ready(function () {
+$(document).ready(function() {
   function getConstellations() {
     $.ajax({
       method: "GET",
